bf55620492dc8f03d1038e74ec536a0ff73b8574
Torin Berg <torinberger@torinberger.com>
2020-10-29 12:37:22 +1000

    [server] Created server side starting template

---
+++ server/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const Koa = require("koa");
+const helmet = require("koa-helmet");
+const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser')
+const app = new Koa();
+
+app.use(helmet());
+app.use(logger());
+app.use(bodyParser());
+
+app.use((ctx) => {
+  ctx.body = "Hello World"
+});
+
+app.listen(4000);
